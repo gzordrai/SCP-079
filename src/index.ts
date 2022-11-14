f93af25bb16148abb93077b3060d7c92323b7aad
Suri-4 <sacha.deliege@gmail.com>
2022-11-14 02:28:01 +0100

    Add bot permission

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { GatewayIntentBits, Partials } from "discord.js";
 import { ExtendedClient } from "./bot";
+import { config } from "dotenv";
+import path from "path";
+config({ path: path.join(__dirname, "../.env") });
 
 const client: ExtendedClient = new ExtendedClient({
     intents: [
