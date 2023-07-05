c33ec4f8389b133e5641abf4fbf0caacc55a18b1
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-07-05 21:58:17 +0200

    Add ready event

---
+++ src/events/ready.ts
@@ -0,0 +1,15 @@
+
+import { Events } from "discord.js";
+import { ExtendedClient, IEvent } from "../bot";
+
+const event: IEvent = {
+    name: Events.ClientReady,
+    once: true,
+    async execute(client: ExtendedClient): Promise<void> {
+        if(!client.user || !client.application) return;
+
+        console.log(`Logged in as ${client.user.tag} !`);
+    },
+};
+
+export default event;
