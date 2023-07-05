2941c6c1be3a862d85815519538472c815ed6990
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-07-05 21:54:41 +0200

    Bot implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { GatewayIntentBits, Partials } from "discord.js";
-import { ExtendedClient } from "./bot";
+import { ExtendedClient, ICommand, IEvent } from "./bot";
 import { config } from "dotenv";
 import path from "path";
+import { readdirSync } from "fs";
+
 config({ path: path.join(__dirname, "../.env") });
 
 const client: ExtendedClient = new ExtendedClient({
@@ -13,3 +15,26 @@ const client: ExtendedClient = new ExtendedClient({
         Partials.Channel
     ]
 });
+const commandsPath: string = path.join(__dirname, "commands");
+const eventsPath: string = path.join(__dirname, "events");
+const commandFiles: Array<string> = readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+const eventFiles: Array<string> = readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath: string = path.join(commandsPath, file);
+    const command: ICommand = require(filePath).default;
+
+    client.commands.set(command.data.name, command);
+}
+
+for (const file of eventFiles) {
+    const filePath: string = path.join(eventsPath, file);
+    const event: IEvent = require(filePath).default;
+
+    if (event.once)
+        client.once(event.name, (...args) => event.execute(client, ...args));
+    else
+        client.on(event.name, (...args) => event.execute(client, ...args));
+}
+
+client.login(process.env.TOKEN);
