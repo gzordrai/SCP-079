025c71bb597446a4d3bb4d44926cdbf187931fb3
Suri-4 <sacha.deliege@gmail.com>
2023-07-05 22:21:41 +0200

    Add hello command

---
+++ src/commands/hello.ts
@@ -0,0 +1,14 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { ICommand } from "../bot";
+
+export const command: ICommand = {
+    data: new SlashCommandBuilder()
+        .setName("hello")
+        .setDescription("test"),
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+
+        await interaction.followUp({content:"Hello"});
+    }
+}
+
+export default command;
