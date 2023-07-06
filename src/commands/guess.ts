6f447398ff3ecdb241ff6db7304fee769d04b0b0
Suri-4 <sacha.deliege@gmail.com>
2023-07-06 22:12:03 +0200

    Update guess command

--- src/commands/guess.ts
+++ src/commands/guess.ts
@@ -3,14 +3,28 @@ import { ICommand } from "../bot";
 
 export const command: ICommand = {
     data: new SlashCommandBuilder()
-        .setName("Guess Number")
-        .setDescription("Guess a number"),
+        .setName("guess")
+        .setDescription("Guess a number")
+        .addIntegerOption(option =>
+            option
+                .setName("input")
+                .setDescription("User value input")
+                .setRequired(true)),
+                
     async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        const user_guess = interaction.options.getInteger("input", true);
+        const random_number: number = Math.floor(Math.random() * 10);
 
-        const random_number = Math.floor(Math.random() * 10);
-
-
-        await interaction.reply({content:"Le nombre est : " +random_number});
+        if (user_guess < 0 || user_guess > 10) {
+            await interaction.followUp({content:"Nombre hors limite"})
+        } else {
+            if (user_guess == random_number) {
+                await interaction.followUp({content:`Félicitation, le nombre était bien : ${random_number}`});
+            }
+            else {
+                await interaction.followUp({content:`Vous n'avez pas mis le bon nombre, le bon nombre était : ${random_number}`});
+            }
+        }
     }
 }
 
